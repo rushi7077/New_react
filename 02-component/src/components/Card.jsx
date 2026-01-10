5732f8f29c6ac2b5ebf023100aa7d77507baae51
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-01-10 22:57:04 +0530

    Updated

--- 02-component/src/components/Card.jsx
+++ 02-component/src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card() {
   return (
     <div className='card'>
-        <h1>Mahebub Shaikh</h1>
+        <h1>Rushikesh Baravkar</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, eligendi! Eaque, temporibus. Ad error ullam, odio expedita voluptatum suscipit minus rem quidem voluptates amet praesentium dolor ducimus similique necessitatibus! Mollitia?</p>
     </div>
   )
