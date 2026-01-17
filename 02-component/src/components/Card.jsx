87def8409a8d46c09b3bc521b51d563359eb9135
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-01-17 19:16:37 +0530

    updated

--- 02-component/src/components/Card.jsx
+++ 02-component/src/components/Card.jsx
@@ -6,6 +6,7 @@ function Card() {
         <h1>Rushikesh Baravkar</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, eligendi! Eaque, temporibus. Ad error ullam, odio expedita voluptatum suscipit minus rem quidem voluptates amet praesentium dolor ducimus similique necessitatibus! Mollitia?</p>
     </div>
+
   )
 }
 
