6959e4a8e0c3a361c5a931175196becdaf6f0a2d
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-01-19 12:01:53 +0530

    updated

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './assets/components/header/Header'
+import Button from './assets/components/button/Button'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
