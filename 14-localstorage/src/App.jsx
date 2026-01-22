5b489622e1d65daa84206e310daf3fa2d60bd6bd
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-01-22 19:33:30 +0530

    updated

--- 14-localstorage/src/App.jsx
+++ 14-localstorage/src/App.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 
 function App() {
+
+  /*localStorage.clear()
+  localStorage.setItem('user','Rushi')
+  localStorage.setItem('age','22')
+  localStorage.setItem('city','Pune')
+  const user = localStorage.getItem('user')
+  console.log(user);
+  localStorage.removeItem('')*/
+
+  const User = {
+    'Name':'Rushikesh',
+    'Age':22,
+    'City':'Wagholi'
+  }
+
+  localStorage.setItem('User',JSON.stringify(User))
+  const UserDetails = JSON.parse(localStorage.getItem('User'))
+  console.log(UserDetails);
+  
+  
+
   return (
     <div>App</div>
   )
