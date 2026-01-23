0fb51cbda234062ed72bab26931c4b820c88890d
Rushi Baravkar <rushibaravkar1012@gmail.com>
2026-01-23 16:39:37 +0530

    updated

--- 16-useeffect/src/App.jsx
+++ 16-useeffect/src/App.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(100)
+
+  const aChanging = ()=>{
+    console.log("A is changing");
+    
+  }
+
+  const bChanging = ()=>{
+    console.log("B is changing");
+    
+  }
+
+  useEffect(()=>{
+    aChanging();
+  },[a])
+
+  useEffect(()=>{
+    bChanging();
+  },[b])
+
   return (
-    <div>App</div>
+    <div>
+      <h1>{a}</h1>
+      <h1>{b}</h1>
+      <button onClick={(()=>{
+        setA(a+1)
+      })}>A</button>
+      <button onClick={(()=>{
+        setB(b+1)
+      })}>B</button>
+    </div>
   )
 }
 
